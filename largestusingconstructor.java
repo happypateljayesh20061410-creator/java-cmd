53db77da658389b2c8711c6f66cc4815f1904fdf
Happy patel <happypateljayesh20061410@gmail.com>
2026-07-26 15:19:04 +0530

    Create largestusingconstructor.java

---
+++ largestusingconstructor.java
@@ -0,0 +1,12 @@
+class largest{
+int num1;
+int num2;
+largest(){
+num1=10;
+num2=20;
+}
+public static void main(String[] args) {
+largest s1=new largest();
+System.out.println(s1.num1>s1.num2);
+}
+}
