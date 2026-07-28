a41b49d064d586be3126da72e82954037caa8065
Happy patel <happypateljayesh20061410@gmail.com>
2026-07-28 11:37:27 +0530

    Add initial type casting example in Java

---
+++ initialtyecasting.java
@@ -0,0 +1,10 @@
+class Main{
+public static void main(String[]args){
+char intial='H';
+int value =(int)intial;
+
+System.out.println("Initial Value="+intial);
+System.out.println("Value="+value);
+}
+}
+
