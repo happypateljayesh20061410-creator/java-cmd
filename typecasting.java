0ab9b99e183814598a8175dd5186de0c87b6de5c
Happy patel <happypateljayesh20061410@gmail.com>
2026-07-28 11:36:38 +0530

    Add typecasting example in typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,10 @@
+class Main{
+public static void main(String[]args){
+double price=99.99;
+int amount =(int)price;
+
+System.out.println("Double Value="+price);
+System.out.println("Integer Value="+amount);
+}
+}
+
